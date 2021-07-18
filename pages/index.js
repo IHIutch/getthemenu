cf4669ab82803bbda6b7063fbe2488249442e72e
IHIutch <jbhutch01@gmail.com>
2021-07-18 10:54:06 -0400

    Registration and log in forms, install Formik

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import Head from 'next/head'
+import NextLink from 'next/link'
+import { Box, Button, Heading, Link } from '@chakra-ui/react'
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>Create Next Apps</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    </div>
+      <Box>
+        <Heading as="h1">GetTheMenu</Heading>
+
+        <NextLink href="/register" passHref>
+          <Button as={Link} colorScheme="blue">
+            Register
+          </Button>
+        </NextLink>
+        <NextLink href="/log-in" passHref>
+          <Button as={Link}>Log In</Button>
+        </NextLink>
+      </Box>
+    </>
   )
 }
---
+++ pages/register.js
@@ -0,0 +1,115 @@
+import React from 'react'
+import {
+  Box,
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Grid,
+  GridItem,
+  Heading,
+  Input,
+} from '@chakra-ui/react'
+import { Formik } from 'formik'
+import Container from '@/components/common/Container'
+import Head from 'next/head'
+
+export default function Register() {
+  return (
+    <>
+      <Head>
+        <title>Register</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Container py="24">
+        <Grid templateColumns={{ md: 'repeat(12, 1fr)' }} gap="6">
+          <GridItem
+            colStart={{ md: '3', xl: '4' }}
+            colSpan={{ md: '8', xl: '6' }}
+          >
+            <Box bg="white" borderWidth="1px" rounded="md" p="8">
+              <Box mb="8">
+                <Heading as="h1">Create Account</Heading>
+              </Box>
+              <Formik
+                initialValues={{ email: '', password: '' }}
+                validate={(values) => {
+                  const errors = {}
+                  if (!values.email) errors.email = 'Required'
+                  if (!values.password) errors.password = 'Required'
+                  return errors
+                }}
+                onSubmit={(values, { setSubmitting }) => {
+                  setTimeout(() => {
+                    alert(JSON.stringify(values, null, 2))
+                    setSubmitting(false)
+                  }, 400)
+                }}
+              >
+                {({
+                  values,
+                  errors,
+                  touched,
+                  handleChange,
+                  handleBlur,
+                  handleSubmit,
+                  isSubmitting,
+                }) => (
+                  <form onSubmit={handleSubmit}>
+                    <Grid gap="6">
+                      <GridItem>
+                        <FormControl
+                          id="email"
+                          isInvalid={errors.email && touched.email}
+                        >
+                          <FormLabel>Email</FormLabel>
+                          <Input
+                            type="email"
+                            name="email"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.email}
+                            isRequired
+                          />
+                          <FormErrorMessage>{errors.email}</FormErrorMessage>
+                        </FormControl>
+                      </GridItem>
+                      <GridItem>
+                        <FormControl
+                          id="password"
+                          isInvalid={errors.password && touched.password}
+                        >
+                          <FormLabel>Password</FormLabel>
+                          <Input
+                            type="password"
+                            name="password"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.password}
+                            isRequired
+                          />
+                          <FormErrorMessage>{errors.password}</FormErrorMessage>
+                        </FormControl>
+                      </GridItem>
+                      <GridItem>
+                        <Button
+                          ml="auto"
+                          type="submit"
+                          colorScheme="blue"
+                          loadingText="Registering..."
+                          isLoading={isSubmitting}
+                        >
+                          Register
+                        </Button>
+                      </GridItem>
+                    </Grid>
+                  </form>
+                )}
+              </Formik>
+            </Box>
+          </GridItem>
+        </Grid>
+      </Container>
+    </>
+  )
+}
