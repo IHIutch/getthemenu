71b938ed83a4e696a5c1ce1f3bd4d88791b10883
IHIutch <jbhutch01@gmail.com>
2021-07-19 18:55:50 -0400

    Make axios functions for menuItems and restaurants

---
+++ utils/axios/menuItems.js
@@ -0,0 +1,49 @@
+import QueryString from 'qs'
+import axios from 'redaxios'
+
+export const getMenuItems = async (params = null) => {
+  try {
+    const { data } = await axios.get(
+      `/api/menuItems?` + QueryString.stringify(params)
+    )
+    return data
+  } catch (err) {
+    throw new Error(err)
+  }
+}
+
+export const getMenuItem = async (id) => {
+  try {
+    const { data } = await axios.get(`/api/menuItems/${id}`)
+    return data
+  } catch (err) {
+    throw new Error(err)
+  }
+}
+
+export const postMenuItem = async (payload) => {
+  try {
+    const { data } = await axios.post(`/api/menuItems`, payload)
+    return data
+  } catch (err) {
+    throw new Error(err)
+  }
+}
+
+export const putMenuItem = async (id, payload) => {
+  try {
+    const { data } = await axios.put(`/api/menuItems/${id}`, payload)
+    return data
+  } catch (err) {
+    throw new Error(err)
+  }
+}
+
+export const deleteMenuItem = async (id) => {
+  try {
+    const { data } = await axios.delete(`/api/menuItems/${id}`)
+    return data
+  } catch (err) {
+    throw new Error(err)
+  }
+}
---
+++ utils/axios/restaurants.js
@@ -0,0 +1,49 @@
+import QueryString from 'qs'
+import axios from 'redaxios'
+
+export const getRestaurants = async (params = null) => {
+  try {
+    const { data } = await axios.get(
+      `/api/restaurants?` + QueryString.stringify(params)
+    )
+    return data
+  } catch (err) {
+    throw new Error(err)
+  }
+}
+
+export const getRestaurant = async (id) => {
+  try {
+    const { data } = await axios.get(`/api/restaurants/${id}`)
+    return data
+  } catch (err) {
+    throw new Error(err)
+  }
+}
+
+export const postRestaurant = async (payload) => {
+  try {
+    const { data } = await axios.post(`/api/restaurants`, payload)
+    return data
+  } catch (err) {
+    throw new Error(err)
+  }
+}
+
+export const putRestaurant = async (id, payload) => {
+  try {
+    const { data } = await axios.put(`/api/restaurants/${id}`, payload)
+    return data
+  } catch (err) {
+    throw new Error(err)
+  }
+}
+
+export const deleteRestaurant = async (id) => {
+  try {
+    const { data } = await axios.delete(`/api/restaurants/${id}`)
+    return data
+  } catch (err) {
+    throw new Error(err)
+  }
+}
