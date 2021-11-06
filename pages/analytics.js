b8da46fcebd59ae54892ede72262b412730f60a9
IHIutch <jbhutch01@gmail.com>
2021-11-06 15:11:56 -0400

    Update analytics page

--- pages/analytics.js
+++ pages/analytics.js
@@ -1,10 +1,24 @@
 import DefaultLayout from '@/layouts/Default'
+import { Container, Flex } from '@chakra-ui/layout'
 import React from 'react'
 
 export default function Analytics() {
   return (
     <>
-      <DefaultLayout />
+      <DefaultLayout>
+        <Container maxW="container.xl" py="8">
+          <Flex
+            bg="white"
+            align="center"
+            justify="center"
+            p="6"
+            rounded="md"
+            shadow="base"
+          >
+            Coming Soon
+          </Flex>
+        </Container>
+      </DefaultLayout>
     </>
   )
 }
