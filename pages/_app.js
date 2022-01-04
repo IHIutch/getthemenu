71db5a3ee88d242682e37a546dfe12d313842454
IHIutch <jbhutch01@gmail.com>
2022-01-04 12:36:50 -0500

    Add fathom analtyics

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,9 @@ import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import customTheme from '@/customTheme'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import * as Fathom from 'fathom-client'
 
 const theme = extendTheme(customTheme)
 
@@ -17,6 +19,25 @@ export default function App({ Component, pageProps }) {
         },
       })
   )
+
+  const router = useRouter()
+
+  useEffect(() => {
+    Fathom.load('DVZFRWML', {
+      includedDomains: ['getthemenu.io'],
+    })
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview()
+    }
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+  }, [router.events])
 
   return (
     <QueryClientProvider client={queryClient}>
