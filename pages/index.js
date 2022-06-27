16bb66b04a3b7ed7495cb6768a36d13ad6747997
JB Hutch <jbhutch01@gmail.com>
2022-06-27 10:57:35 -0400

    Update homepage

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,24 @@ import supabase from '@/utils/supabase'
 import SEO from '@/components/global/SEO'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
-import { Box, Button, Link, Stack, Text } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Center,
+  Circle,
+  Container,
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  Icon,
+  Link,
+  SimpleGrid,
+  Stack,
+  Text,
+} from '@chakra-ui/react'
 import NextLink from 'next/link'
+import { Check, CheckCircle } from 'lucide-react'
 
 export default function Homepage() {
   const router = useRouter()
@@ -28,6 +44,15 @@ export default function Homepage() {
     }
   }, [router])
 
+  const features = [
+    '210+ Components',
+    'Unlimited projects',
+    'Unlimited users',
+    'Lifetime access',
+    'Customer support',
+    'Free updates',
+  ]
+
   return (
     <>
       <Head>
@@ -39,23 +64,120 @@ export default function Homepage() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Box>
-        <Text>Homepage</Text>
-        <Stack direction="row">
-          <NextLink href="/login" passHref>
-            <Button as={Link}>Log In</Button>
-          </NextLink>
-          <NextLink href="/register" passHref>
-            <Button as={Link}>Sign Up</Button>
-          </NextLink>
-          <NextLink href="/terms-of-use" passHref>
-            <Link>Terms of Use</Link>
-          </NextLink>
-          <NextLink href="/privacy-policy" passHref>
-            <Link>Privacy Policy</Link>
-          </NextLink>
-        </Stack>
-      </Box>
+      <Container h="full" maxW="container.lg">
+        <Flex h="full" direction="column">
+          <Box py="12">
+            <Flex>
+              <Box mb="12">
+                <Heading as="h1" mb="2" fontSize="5xl" fontWeight="semibold">
+                  GetTheMenu
+                </Heading>
+                <Text fontSize="2xl" color="gray.600">
+                  The platform built for both restaurants and their customers
+                </Text>
+              </Box>
+              <Box ml="auto">
+                <NextLink href="/login" passHref>
+                  <Button as={Link}>Log In</Button>
+                </NextLink>
+              </Box>
+            </Flex>
+            <Grid templateColumns="repeat(12, 1fr)" gap="8">
+              <GridItem colSpan={{ base: '12', lg: '6' }}></GridItem>
+              <GridItem colSpan={{ base: '12', lg: '6' }}>
+                <Box bg="white" rounded="lg" shadow="base" w="full">
+                  <Flex p="6" align="center" borderBottomWidth="1px">
+                    <Box>
+                      <Heading
+                        as="h2"
+                        fontSize="2xl"
+                        fontWeight="semibold"
+                        mb="1"
+                      >
+                        Monthly Subscription
+                      </Heading>
+                      <Text color="gray.600">
+                        1 month{' '}
+                        <Text as="span" fontWeight="semibold">
+                          FREE
+                        </Text>{' '}
+                        trial, no credit card required!
+                      </Text>
+                    </Box>
+                    <Box ml="auto" pl="6">
+                      <Flex align="flex-end">
+                        <Flex align="flex-start">
+                          <Text fontSize="3xl" fontWeight="bold" mt="3">
+                            $
+                          </Text>
+                          <Text fontSize="6xl" fontWeight="medium">
+                            20
+                          </Text>
+                        </Flex>
+                        <Text mb="5" whiteSpace="nowrap">
+                          / month
+                        </Text>
+                      </Flex>
+                    </Box>
+                  </Flex>
+                  <Box p="6" borderBottomWidth="1px">
+                    <Box mb="8">
+                      <Heading
+                        as="h3"
+                        fontSize="lg"
+                        fontWeight="semibold"
+                        mb="1"
+                      >
+                        What&apos;s Included?
+                      </Heading>
+                      <Text color="gray.600">
+                        All you need to build your restuarant website!
+                      </Text>
+                    </Box>
+                    <SimpleGrid columns={2} spacing="4">
+                      {features.map((feature, idx) => (
+                        <GridItem
+                          key={idx}
+                          as={Stack}
+                          direction="row"
+                          alignItems="center"
+                        >
+                          <Circle bg="green.50" boxSize="6" p="1">
+                            <Icon color="green.600" as={Check} boxSize="full" />
+                          </Circle>
+                          <Text>{feature}</Text>
+                        </GridItem>
+                      ))}
+                    </SimpleGrid>
+                  </Box>
+                  <Box p="6">
+                    <NextLink href="/register" passHref>
+                      <Button
+                        as={Link}
+                        colorScheme="blue"
+                        size="lg"
+                        isFullWidth
+                      >
+                        Sign Up Today!
+                      </Button>
+                    </NextLink>
+                  </Box>
+                </Box>
+              </GridItem>
+            </Grid>
+          </Box>
+          <Center mt="auto" w="full" py="4">
+            <Stack direction="row" spacing="6">
+              <NextLink href="/terms-of-use" passHref>
+                <Link>Terms of Use</Link>
+              </NextLink>
+              <NextLink href="/privacy-policy" passHref>
+                <Link>Privacy Policy</Link>
+              </NextLink>
+            </Stack>
+          </Center>
+        </Flex>
+      </Container>
     </>
   )
 }
