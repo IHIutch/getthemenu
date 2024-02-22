55c62ea81c987d984512833d5175d0fdf37ac9c6
JB Hutch <jbhutch01@gmail.com>
2024-02-22 15:02:02 -0500

    Remove unnecessary axios functions and api endpoints

--- utils/axios/feedback.js
+++
@@ -1,8 +0,0 @@
-import axios from 'redaxios'
-
-export const postFeedback = async (payload) => {
-  const { data } = await axios.post(`/api/feedback`, payload).catch((res) => {
-    throw new Error(res.data.error)
-  })
-  return data
-}
