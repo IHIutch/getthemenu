7d4f8217eb03cc5d825a8e7cae41335894474fec
Jonathan Hutchison <jbhutch01@gmail.com>
2025-06-10 17:13:07 -0400

    Move prisma client directly into index

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,2 +1,25 @@
-export * from '../generated/client'
-export { prisma } from './client'
+import process from 'node:process'
+
+import { PrismaClient } from '../generated/client/index.js'
+// import { withAccelerate } from '@prisma/extension-accelerate'
+
+function prismaClientSingleton() {
+  return new PrismaClient({
+    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+  })
+  // .$extends(withAccelerate())
+}
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined
+}
+
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
+
+export * from '../generated/client/index.js'
