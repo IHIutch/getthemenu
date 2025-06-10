cfe2127aaefb01b39ea36bc4a8582592af25f07d
Jonathan Hutchison <jbhutch01@gmail.com>
2025-06-10 17:33:11 -0400

    Use prisma-client-js

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,25 +1,2 @@
-import process from 'node:process'
-
-import { PrismaClient } from '../generated/client/index.js'
-// import { withAccelerate } from '@prisma/extension-accelerate'
-
-function prismaClientSingleton() {
-  return new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-  })
-  // .$extends(withAccelerate())
-}
-
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined
-}
-
-export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
-
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma
-}
-
-export * from '../generated/client/index.js'
+export { prisma } from './client'
+export * from '@prisma/client'
