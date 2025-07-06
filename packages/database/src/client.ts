788787cc983ef3d684a646fd6d7fe6dec972cc49
Jonathan Hutchison <jbhutch01@gmail.com>
2025-07-06 11:30:19 -0400

    Use pg adapter

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,11 +1,17 @@
+import { PrismaPg } from '@prisma/adapter-pg'
 import process from 'node:process'
 
 import { PrismaClient } from './generated/prisma/client'
+
 // import { withAccelerate } from '@prisma/extension-accelerate'
 
 function prismaClientSingleton() {
+  const adapter = new PrismaPg({
+    connectionString: process.env.POSTGRES_PRISMA_URL,
+  })
   return new PrismaClient({
     log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+    adapter,
   })
   // .$extends(withAccelerate())
 }
